de004ed58daefded30ade38ccff46629787496b6
Emily Emmons <emily@logicbranch.com>
2018-12-02 12:48:02 -0500

    added navigation controller to storyboard

---
+++ 491TestRepo/TestPageViewController.swift
@@ -0,0 +1,98 @@
+//
+//  TestStartViewController.swift
+//  491TestRepo
+//
+//  Created by Emily Emmons on 12/2/18.
+//  Copyright © 2018 Emily Emmons. All rights reserved.
+//
+
+import UIKit
+
+class TestPageViewController: UIViewController {
+    
+    @IBOutlet weak var testNameLabel: UILabel!
+    
+    @IBOutlet weak var startTestButton: UIButton!
+    
+    var currTestName = ""
+    //get tests from buttons
+    let testName = "tremor"
+    switch testName {
+        case "tremor":
+            let text = """
+                        Tremor
+                        """
+            testNameLabel.text = text
+        case "bradykinesia":
+            let text = """
+                        Bradykinesia
+                        """
+            testNameLabel.text = text
+        case "balance":
+            let text = """
+                        Balance
+                        """
+            testNameLabel.text = text
+        case "cognitive":
+            let text = """
+                        Cognitive
+                        """
+            testNameLabel.text = text
+        case "rest tremor":
+            let text = """
+                        Rest Tremor
+                        """
+            testNameLabel.text = text
+        case "postural tremor":
+            let text = """
+                        Postural Tremor
+                        """
+            testNameLabel.text = text
+        case "intention tremor":
+            let text = """
+                        Intention Tremor
+                        """
+            testNameLabel.text = text
+        case "kinetic tremor":
+            let text = """
+                        Kinetic Tremor
+                        """
+            testNameLabel.text = text
+        case "rapid alternating movements":
+            let text = """
+                        Rapid Alternating Movements
+                        """
+            testNameLabel.text = text
+        case "finger tapping":
+            let text = """
+                        Finger Tapping
+                        """
+            testNameLabel.text = text
+        default:
+            let text = "No test selected"
+            statusLabel.text = text
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
